ac4c306c71a50493891e8c59be517e970262b9a1
Simon Liang <simon@x-tech.io>
2023-04-24 21:44:15 +0800

    added nohoist

--- packages/padlocal/src/queue/queue.module.ts
+++ packages/padlocal/src/queue/queue.module.ts
@@ -1,6 +1,6 @@
 import { BullModule } from '@nestjs/bullmq';
 import { forwardRef, Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { StorageModule } from '@senses-chat/padlocal-db';
 
 import { processors } from './processor';
