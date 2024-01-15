4b83fe95d0313a634ad723336adcf140561b6330
Simon Liang <simon@x-tech.io>
2024-01-15 18:43:28 +0800

    got most of dependencies fixed

--- src/padlocal/padlocal.module.ts
+++ src/padlocal/padlocal.module.ts
@@ -1,16 +1,57 @@
 import { Module } from '@nestjs/common';
+import { BullModule } from '@nestjs/bullmq';
+import { BullBoardModule } from '@bull-board/nestjs';
+import { BullMQAdapter } from '@bull-board/api/bullMQAdapter';
 
 import { RedisModule } from '@/modules/redis';
 import { MinioModule } from '@/modules/minio';
 import { OpenSearchModule } from '@/modules/opensearch';
+import { DrizzleModule } from '@/modules/drizzle';
 
 import { processors } from './processors';
 import { PadlocalController } from './padlocal.controller';
 import { PadlocalService } from './padlocal.service';
 import { MessageParserService } from './parser.service';
+import {
+  ACTIONS_OPTIONS,
+  FRIEND_REQUEST_OPTIONS,
+  KICK_OUT_OPTIONS,
+  LOGIN_QR_CODE_OPTIONS,
+  LOGIN_START_OPTIONS,
+  LOGIN_SUCCESS_OPTIONS,
+  NEW_FRIEND_REQUEST_OPTIONS,
+  NEW_MESSAGE_OPTIONS,
+  NEW_RAW_MESSAGE_OPTIONS,
+  SYNC_CONTACT_OPTIONS,
+} from './queues';
+
+const queues = [
+  ACTIONS_OPTIONS,
+  KICK_OUT_OPTIONS,
+  LOGIN_QR_CODE_OPTIONS,
+  LOGIN_START_OPTIONS,
+  LOGIN_SUCCESS_OPTIONS,
+  NEW_FRIEND_REQUEST_OPTIONS,
+  FRIEND_REQUEST_OPTIONS,
+  NEW_MESSAGE_OPTIONS,
+  NEW_RAW_MESSAGE_OPTIONS,
+  SYNC_CONTACT_OPTIONS,
+];
 
 @Module({
-  imports: [RedisModule, MinioModule, OpenSearchModule],
+  imports: [
+    RedisModule,
+    MinioModule,
+    OpenSearchModule,
+    DrizzleModule,
+    BullModule.registerQueue(...queues),
+    BullBoardModule.forFeature(
+      ...queues.map((queue) => ({
+        name: queue.name,
+        adapter: BullMQAdapter,
+      })),
+    ),
+  ],
   controllers: [PadlocalController],
   providers: [PadlocalService, MessageParserService, ...processors],
   exports: [PadlocalService, MessageParserService],
---
+++ src/padlocal/queues.ts
@@ -0,0 +1,88 @@
+import { RegisterQueueOptions } from '@nestjs/bullmq';
+
+export const ACTIONS = 'actions';
+export const KICK_OUT = 'kick-out';
+export const LOGIN_QR_CODE = 'login-qr-code';
+export const LOGIN_START = 'login-start';
+export const LOGIN_SUCCESS = 'login-success';
+export const NEW_FRIEND_REQUEST = 'new-friend-request';
+export const FRIEND_REQUEST = 'friend-request';
+export const NEW_MESSAGE = 'new-message';
+export const NEW_RAW_MESSAGE = 'new-raw-message';
+export const SYNC_CONTACT = 'sync-contact';
+
+const streams: RegisterQueueOptions['streams'] = {
+  events: {
+    maxLen: 100,
+  },
+};
+
+const defaultJobOptions: RegisterQueueOptions['defaultJobOptions'] = {
+  removeOnComplete: false,
+  removeOnFail: false,
+  attempts: 10,
+  backoff: {
+    type: 'exponential',
+    delay: 1000,
+  },
+};
+
+export const ACTIONS_OPTIONS: RegisterQueueOptions = {
+  name: ACTIONS,
+  streams,
+  defaultJobOptions,
+};
+
+export const KICK_OUT_OPTIONS: RegisterQueueOptions = {
+  name: KICK_OUT,
+  streams,
+  defaultJobOptions,
+};
+
+export const LOGIN_QR_CODE_OPTIONS: RegisterQueueOptions = {
+  name: LOGIN_QR_CODE,
+  streams,
+  defaultJobOptions,
+};
+
+export const LOGIN_START_OPTIONS: RegisterQueueOptions = {
+  name: LOGIN_START,
+  streams,
+  defaultJobOptions,
+};
+
+export const LOGIN_SUCCESS_OPTIONS: RegisterQueueOptions = {
+  name: LOGIN_SUCCESS,
+  streams,
+  defaultJobOptions,
+};
+
+export const NEW_FRIEND_REQUEST_OPTIONS: RegisterQueueOptions = {
+  name: NEW_FRIEND_REQUEST,
+  streams,
+  defaultJobOptions,
+};
+
+export const FRIEND_REQUEST_OPTIONS: RegisterQueueOptions = {
+  name: FRIEND_REQUEST,
+  streams,
+  defaultJobOptions,
+};
+
+export const NEW_MESSAGE_OPTIONS: RegisterQueueOptions = {
+  name: NEW_MESSAGE,
+  streams,
+  defaultJobOptions,
+};
+
+export const NEW_RAW_MESSAGE_OPTIONS: RegisterQueueOptions = {
+  name: NEW_RAW_MESSAGE,
+  streams,
+  defaultJobOptions,
+};
+
+export const SYNC_CONTACT_OPTIONS: RegisterQueueOptions = {
+  name: SYNC_CONTACT,
+  streams,
+  defaultJobOptions,
+};
